ce09c36c124eb45ca80b5cc899423f620f45b3ee
roman-245 <114242970+roman-245@users.noreply.github.com>
2024-10-30 23:52:36 +0300

    Add files via upload

--- src/main/resources/static/gist.js
+++ src/main/resources/static/gist.js
@@ -1,22 +1,47 @@
-google.charts.load('current', {packages: ['corechart']});
-google.charts.setOnLoadCallback(drawChart);
+document.addEventListener("DOMContentLoaded", function () {
+    google.charts.load('current', {'packages':['corechart']});
+    google.charts.setOnLoadCallback(drawChart);
 
-function drawChart() {
-    var data = google.visualization.arrayToDataTable([
-        ['Year', 'Sales', 'Expenses'],
-        ['2013', 1000, 400],
-        ['2014', 1170, 460],
-        ['2015', 660, 1120],
-        ['2016', 1030, 540]
-    ]);
+    function drawChart() {
+        // Используем данные, которые переданы через Thymeleaf
+        const perfData = ThData;
 
-    var options = {
-        title: 'Company Performance',
-        curveType: 'function',
-        legend: { position: 'bottom' }
-    };
+        // Создаем объект для хранения количества данных по каждой дате отправки
+        const dataCountByDate = {};
 
-    var chart = new google.visualization.LineChart(document.getElementById('curve_chart'));
+        // Пробегаем по данным и группируем их по дате отправки
+        perfData.forEach(function(perf) {
+            const count_date = new Date(perf.date).toLocaleDateString();  // Преобразуем дату в формат строки
 
-    chart.draw(data, options);
-}
+            // Если дата уже существует в объекте, увеличиваем количество грузов, иначе создаем новый ключ
+            if (dataCountByDate[count_date]) {
+                dataCountByDate[count_date]++;
+            } else {
+                dataCountByDate[count_date] = 1;
+            }
+        });
+
+        // Преобразуем объект в массив для диаграммы
+        const dataArray = [['Дата', 'Количество билетов']];
+        for (const [date, count] of Object.entries(dataCountByDate)) {
+            dataArray.push([date, count]);
+        }
+
+        // Преобразуем массив данных в формат DataTable для Google Charts
+        const data = google.visualization.arrayToDataTable(dataArray);
+
+        // Настройки диаграммы
+        const options = {
+            title: 'Количество билетов по дате',
+            hAxis: {title: 'Дата'},
+            vAxis: {title: 'Количество'},
+            legend: {position: 'none'},
+            bar: {groupWidth: "95%"},
+            colors: ['#76A7FA'],
+        };
+
+        // Создаем и рисуем диаграмму
+        const chart = new google.visualization.ColumnChart(document.getElementById('curve_chart'));
+        chart.draw(data, options);
+    }
+});
